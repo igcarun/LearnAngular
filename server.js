b645d6a199539d970053a63cc7eebbbe1d9ac01c
Arunkumar Ramasamy <igcarun@gmail.com>
2019-07-07 18:06:52 +0530

    update server js file

--- server.js
+++ server.js
@@ -5,12 +5,22 @@ const path = require('path');
 
 const app = express();   
 
-// Serve only the static files form the dist directory    
-app.use(express.static(__dirname + '/dist'));
+const port = process.env.PORT || 8000;
+const server = require('http').Server(app);
 
-app.get('/*', function(req,res) {  
-    res.sendFile(path.join(__dirname+'/dist/learnDirective/index.html'));   
-});  
+app.use(express.static(__dirname, 'dist', {index: false}));
 
-// Start the app by listening on the default Heroku port    
-app.listen(process.env.PORT || 8080);
+
+server.listen(port, function() {
+    console.log("App running on port " + port);
+})
+
+// PathLocationStrategy
+
+app.get('', function(req, res) {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
